a0fffded75310ceb6f3cbe60123be992bbba85ac
shamim-basha <shamimbasha13@gmail.com>
2025-08-06 20:25:36 +0530

    Setup

---
+++ Backend/src/main/java/com/SRVK/Hardware/controller/test.java
@@ -0,0 +1,14 @@
+package com.SRVK.Hardware.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class test {
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World!";
+    }
+}
