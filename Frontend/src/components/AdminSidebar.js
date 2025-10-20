f9967b460a014b7686af91e5312f492edc288236
shamim-basha <shamimbasha13@gmail.com>
2025-10-20 12:56:37 +0530

    Added link to navigate to home

--- Frontend/src/components/AdminSidebar.js
+++ Frontend/src/components/AdminSidebar.js
@@ -27,7 +27,7 @@ const AdminSidebar = () => {
   return (
     <>
     <aside className="admin-sidebar">
-      <div className="sidebar-logo">Lanka Hardware</div>
+      <div className="sidebar-logo" onClick={navigate("/")}>Lanka Hardware</div>
       <nav className="sidebar-nav">
         <ul>
           <Link to="/admin" className="sidebar-link" style={{ textDecoration: 'none' }}>
