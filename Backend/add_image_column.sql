886dbbffe9d36404b6f827a97b6ef2183441a84b
shamim-basha <shamimbasha13@gmail.com>
2025-10-20 16:05:26 +0530

    Deleted unwanted file

--- Backend/add_image_column.sql
+++
@@ -1,8 +0,0 @@
--- Add image column to existing tools table
--- Run this in phpMyAdmin SQL tab
-
--- Add image column to tools table
-ALTER TABLE tools ADD COLUMN image LONGBLOB;
-
--- Verify the column was added
-DESCRIBE tools;
